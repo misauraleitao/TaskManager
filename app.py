08893f2b14df9f7f9c6ab7014f11531628f4ce45
misauraleitao <misauraleitao@gmail.com>
2021-09-23 13:01:43 +0000

    Delete Catagory Functionality

--- app.py
+++ app.py
@@ -173,6 +173,12 @@ def edit_catagory(catagory_id):
     catagory = mongo.db.catagories.find_one({"_id": ObjectId(catagory_id)})
     return render_template('edit_catagory.html', catagory=catagory)
 
+@app.route("/delete_catagory<catagory_id>")
+def delete_catagory(catagory_id):
+    mongo.db.catagories.remove({'_id':ObjectId(catagory_id)})
+    flash("Category Successfully Deleted")
+    return redirect(url_for('get_catagories'))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
